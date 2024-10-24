d1144101bab4dced326133a85aed4a32144ae2bc
gspannu <gspannu@msn.com>
2024-10-24 15:30:57 +0100

    Version 1.3.9, Release 0

---
+++ controld/src/opnsense/mvc/app/controllers/OPNsense/ControlD/Api/GeneralController.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * Copyright (C) 2024 Guru Pannu <opnsense@gurupannu.com>
+ * All rights reserved.
+ */
+
+namespace OPNsense\ControlD\Api;
+
+use OPNsense\Base\ApiMutableModelControllerBase;
+
+class GeneralController extends ApiMutableModelControllerBase
+{
+    protected static $internalModelClass = '\OPNsense\ControlD\General';
+    protected static $internalModelName = 'general';
+}
